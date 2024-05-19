633965f0dd8b818f9fa0a082e303b4cabff03f9d
yewchung <philipchengreen@yahoo.com>
2024-05-19 16:55:27 -0700

    Added Not The Binary Light

--- Assets/Mod Scripts/NotTheBinaryLight.cs
+++ Assets/Mod Scripts/NotTheBinaryLight.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 using KModkit;
 using Rnd = UnityEngine.Random;
 
-public class TheBinaryLight : MonoBehaviour {
+public class NotTheBinaryLight : MonoBehaviour {
 
    public KMBombInfo Bomb;
    public KMAudio Audio;
@@ -41,19 +41,11 @@ public class TheBinaryLight : MonoBehaviour {
    private bool needsHolds = true;
 	 private bool hoverEnabled = false;
 
-
-   private int binary = 0;
-	 private int[] position = new int[2];
-	 private int[] answers = new int[7];
-	 private int questions = 0;
-
-
-	 private String[,] questionsText = {
-		 	{"Serial number contains A, C, or D.", "RCA port is present.", "BOB indicator is present.", "More battery holders than strikes."},
-			{"One or more AA batteries.", "Indicator containing R, C, or A.", "Product of all digits in serial number is even.", "Bomb has lit CAR, NSA, or DVI port."},
-			{"Parallel port is present.", "Serial number contains R, J, 4, or 5.", "Number of vowels in all indicator labels is even.", "Have answered at least 4 questions."},
-			{"More port plates than D batteries.", "Sum of serial number digits is a multiple of number of battery holders.", "Answered yes to the question before last.", "More yes answers so far than no."}
-	 };
+	 private int binary = 0;
+   private int number = 1;
+	 private int goal;
+	 private int swap = 0;
+	 private string[] lightfunctions = {"-1", "x2"};
 
    void Awake () {
       ModuleId = ModuleIdCounter++;
@@ -117,7 +109,7 @@ public class TheBinaryLight : MonoBehaviour {
        LightColorChange(hoveredColor);
      } else {
 		 	if (binary == 1) {
-				color = 0;
+				color = 7;
 			} else if (binary == 0){
 				color = -1;
 			}
@@ -126,33 +118,9 @@ public class TheBinaryLight : MonoBehaviour {
    }
 
    void SetupModule() {
-		 if (Bomb.GetBatteryCount() % 2 == 1) {
-			 position[0] = 0;
-		 } else if (Bomb.GetOnIndicators().Count() > Bomb.GetOffIndicators().Count()) {
-			 position[0] = 1;
-		 } else if (Bomb.GetPortCount() > Bomb.GetPortPlateCount()) {
-			 position[0] = 2;
-		 } else if (Bomb.GetTime() >= 0){
-			 position[0] = 3;
-		 } else {
-			 position[0] = 4;
-		 }
-		 if (Bomb.GetSerialNumberNumbers().Last() % 2 == 0) {
-			 position[1] = 0;
-		 } else if (Bomb.GetSerialNumber().Any(ch => "AEIOU".Contains(ch))) {
-			 position[1] = 1;
-		 } else if (Bomb.GetSerialNumberNumbers().Count() >= 2) {
-			 position[1] = 2;
-		 } else if (Bomb.GetSerialNumber().Any(ch => "Y".Contains(ch))) {
-			 position[1] = 3;
-		 } else {
-			 position[1] = 4;
-		 }
-		 if(position[0] == 4 || position[1] == 4) {
-			 DebugMessage("Final starting conditions failed somehow, any answer will solve module.");
-		 } else {
-			 DebugMessage("Initial column is " + (position[0] + 1).ToString() + ", intial row is " + (position[1] + 1).ToString());
-		 }
+		 goal = Bomb.GetSerialNumberNumbers().Aggregate(0, (x, y) => (x * 10) + y);
+		 DebugMessage("Goal number is " + goal.ToString());
+		 DebugMessage("Light off is " + lightfunctions[swap] + ", Light on is " + lightfunctions[(swap+1)%2]);
    }
 
 	 protected void ButtonTap() {
@@ -192,109 +160,39 @@ public class TheBinaryLight : MonoBehaviour {
    }
 
    protected void ButtonInteract2(int i = 0) {
-		 if (position[0] == 4 || position[1] == 4) {
-			 color = 4;
-			 GetComponent<KMBombModule>().HandlePass();
-			 DebugMessage("Module solved... somehow.");
+		 if((swap == 0 && binary == 0) || (swap == 1 && binary == 1)) {
+			 number -= 1;
+		 } else if ((swap == 0 && binary == 1) || (swap == 1 && binary == 0)) {
+			 number *= 2;
 		 }
-     DebugMessage("Current question is \"" + questionsText[position[1], position[0]] + "\"");
-		 bool answer = false;
-		 switch(position[1]) {
-			 case 0:
-			 		switch(position[0]) {
-						case 0:
-							answer = Bomb.GetSerialNumber().Any(ch => "ACD".Contains(ch));
-						break;
-						case 1:
-							answer = Bomb.IsPortPresent(Port.StereoRCA);
-						break;
-						case 2:
-							answer = Bomb.IsIndicatorPresent(Indicator.BOB);
-						break;
-						case 3:
-							answer = Bomb.GetBatteryHolderCount() > Bomb.GetStrikes();
-						break;
-					}
-			 break;
-			 case 1:
-			 		switch(position[0]) {
-						case 0:
-							answer = Bomb.GetBatteryCount(Battery.AA) + Bomb.GetBatteryCount(Battery.AAx3) + Bomb.GetBatteryCount(Battery.AAx4) >= 1;
-						break;
-						case 1:
-							answer = Bomb.GetIndicators().Any(str => "RCA".ToArray().Intersect(str.ToArray()).Count() > 0);
-						break;
-						case 2:
-							answer = Bomb.GetSerialNumberNumbers().Aggregate(1, (x,y) => x * y) % 2 == 0;
-						break;
-						case 3:
-							answer = Bomb.IsIndicatorOn(Indicator.CAR) || Bomb.IsIndicatorOn(Indicator.NSA) || Bomb.IsPortPresent(Port.DVI);
-						break;
-					}
-			 break;
-			 case 2:
-			 		switch(position[0]) {
-						case 0:
-							answer = Bomb.IsPortPresent(Port.Parallel);
-						break;
-						case 1:
-							answer = Bomb.GetSerialNumber().Any(ch => "RJ45".Contains(ch));
-						break;
-						case 2:
-							answer = Bomb.GetIndicators().Aggregate(0, (x, y) => x + "AEIOU".ToArray().Intersect(y.ToArray()).Count()) % 2 == 0;
-						break;
-						case 3:
-							answer = questions >= 4;
-						break;
-					}
-			 break;
-			 case 3:
-			 		switch(position[0]) {
-						case 0:
-							answer = Bomb.GetPortPlateCount() > Bomb.GetBatteryCount(Battery.D);
-						break;
-						case 1:
-							answer = Bomb.GetBatteryCount() > 0 && Bomb.GetSerialNumberNumbers().Sum() % Bomb.GetBatteryHolderCount() == 0;
-						break;
-						case 2:
-							answer = questions > 1 && answers[questions - 2] == 1;
-						break;
-						case 3:
-							answer = answers.Sum() > (questions / 2);
-						break;
-					}
-					break;
-				}
-			 String[] truthiness = {"False", "True"};
-			 int answerInt = answer ? 1 : 0;
-			 DebugMessage("Correct answer should be " + truthiness[answerInt]);
-			 DebugMessage("Submitted answer is " + truthiness[binary]);
-
-			 if (binary == answerInt) {
-				 if (position[0] == 3 && position[1] == 3) {
-					 binary = 2;
-					 color = 4;
-					 DebugMessage("Module solved!");
-					 GetComponent<KMBombModule>().HandlePass();
-					 ModuleSolved = true;
-				 } else {
-					 StartCoroutine(GreenFlash());
-					 if ((answerInt == 0 && position[0] < 3) || position[1] == 3) {
-						 position[0] += 1;
-					 } else if ((answerInt == 1 && position[1] < 3) || position[0] == 3) {
-						 position[1] += 1;
-					 }
-					 answers[questions] = answerInt;
-					 questions += 1;
-					 DebugMessage("Answered correctly, moved to question " + (position[0] + 1).ToString() + ", " + (position[1] + 1).ToString());
-					 binary = 0;
-				 }
-			 } else {
-				 DebugMessage("Answered incorrectly. Strike incurred.");
-				 GetComponent<KMBombModule>().HandleStrike();
-				 StartCoroutine(RedFlash());
-				 binary = 0;
+		 if ((number < 1 && goal != 0) || number > (goal + 1)) {
+			 DebugMessage("Number left allowed bounds, strike incurred.");
+			 DebugMessage("Number reset to 1, light states returned to default functions");
+			 GetComponent<KMBombModule>().HandleStrike();
+			 StartCoroutine(RedFlash());
+			 number = 1;
+			 swap = 0;
+			 binary = 0;
+		 } else {
+			 DebugMessage("Number changed to " + number.ToString());
+			 var numberstr = (number * number).ToString();
+			 int a = (numberstr.Length / 2);
+			 int n = (numberstr.ToArray()[a]) - '0';
+			 if (n % 2 == 1) {
+				 swap = (swap + 1) % 2;
+				 DebugMessage("Light roles swapped.");
+				 DebugMessage("Light off is " + lightfunctions[swap] + ", Light on is " + lightfunctions[(swap+1)%2]);
 			 }
+			 if(number == goal) {
+				 DebugMessage("Goal number reached, module solved!");
+				 GetComponent<KMBombModule>().HandlePass();
+				 binary = 2;
+				 color = 4;
+				 ModuleSolved = true;
+			 } else {
+				 StartCoroutine(GreenFlash());
+			 }
+		 }
    }
 
    void TapTimerFinished(System.Object source, System.Timers.ElapsedEventArgs e) {
@@ -366,6 +264,6 @@ public class TheBinaryLight : MonoBehaviour {
 	 }
 
    void DebugMessage(string message) {
-		 Debug.LogFormat("[The Binary Light #{0}] {1}", ModuleId, message);
+		 Debug.LogFormat("[Not The Binary Light #{0}] {1}", ModuleId, message);
 	 }
 }
